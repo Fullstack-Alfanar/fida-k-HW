791146af764c1f58d23679f4cc242315c62ac1dc
YouKnowWho24 <62944253+YouKnowWho24@users.noreply.github.com>
2022-03-27 22:12:47 +0300

    all questoins without the challenges is done

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # fidas-HW
+## ** question number 3**
+### ask to print first and last cell, the array itself and the oppsite direction of the array.
+## ** question number 2**
+### create a fruit array (size 4), print each fruit in a line with loop and once without loop.
+## ** question number 3**
+### create an array named grades with filled by the user then we print if the grade is valid (between 0-100)
+## ** question number 4**
+### create an array named numbers which contain 20 number between 0-100 (included) we have to print the 
+> #### 1) the opsite direction of the array 
+> #### 2)the sum of all numbers in array
+> #### 3)the average of all numbers in array
+> #### 4)the max number in array
+> #### 5)the min number in array
+## ** question number 20**
+### print a tringle of number with size that we took from user 
+## ** question number 21**
+### print a square of number with size that we took from user 
+## ** question number 22**
+### save all number that user give until it takes zero then print all numbers oppsite way
+
+## **Challenge question number 1**
+
+## **Challenge question number 2**
+
+## **Challenge question number 3**
--- hw-26/03/arrays.js
+++ hw-26/03/arrays.js
@@ -1,19 +1,106 @@
 
 /*question number 1 */
-var arr = [];
-var num;
+// /* var names = [];
+// var num;
 
 
-for (var i = 0; i < 5; i++) {
-    arr[i] = window.prompt("please Enter a name:");
+// for (var i = 0; i < 5; i++) {
+//     names[i] = window.prompt("please Enter a name:");
 
+// }
+// console.log("the first cell in the array " + names[0]);
+// console.log("the last cell in the array " + names[4]);
+// console.log("the array is :")
+// for (var i = 0; i < 5; i++)
+//     console.log(names[i]);
+
+// console.log("the array in the oppsite is :")
+// console.log(names[4] + names[3] + names[2] + names[1] + names[0]);
+
+
+// /*question 2 */
+// var fruits = ["apple", "pear", "banana", "dates"];
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+// console.log("printing with help of a loop");
+// for (i = 0; i < 4; i++)
+//     console.log(fruits[i]);
+
+// /*question 3 */
+// var grades = [];
+// flag = 1;
+// for (i = 0; i < 10; i++) {
+//     grades[i] = window.prompt("enter garde please");
+//     if (grades[i] < 0 || grades[i] > 100) {
+//         console.log("this grade " + grades[i] + " isn't valid");
+//         flag = 0;
+//     }
+// }
+// if (flag == 1)
+//     console.log("all gardes are valid");
+
+// /*question 4 */
+// var numbers = [];
+
+// var sum = 0, avg = 0, max = -1; min = 101;
+// /*inputing the valid numbers from user */
+// for (i = 0; i < 20; i++) {
+//     numbers[i] = +prompt("enter number between 0-100 included :");
+//     while (numbers[i] < 0 || numbers[i] > 100)
+//         numbers[i] = +prompt("please enter a valid number between 0-100(included) :");
+// }
+// /*printing oppsite dicretion of an array, calc the sum finding the max and min */
+// console.log("array")
+// for (i = 19; i >= 0; i--) {
+//     console.log(numbers[i]);
+//     sum = sum + numbers[i];
+//     if (numbers[i] > max)
+//         max = numbers[i];
+//     if (min > numbers[i])
+//         min = numbers[i];
+// }
+
+// /*printing the sum of all number  */
+// console.log("the sum of all number in array " + sum);
+// /*printing the average of all number  */
+// avg = sum / 20;
+// console.log("the average of all number in array " + avg);
+// /*printing the max number in the array  */
+// console.log("the max number in the array " + max);
+// /*printing the min number in the array  */
+// console.log("the max number in the array " + min);
+
+
+// /*question 20 */
+// var n = +prompt("please enter a size:");
+// var tringle = [];
+
+// for (i = 0; i < n; i++) {
+//     tringle[i] = i + 1;
+//     console.log(tringle);
+// }
+
+// /*question 21 */
+// var n = +prompt("please enter a size:");
+// var tringle = [];
+
+// for (var i = 0; i < n; i++) {
+//     tringle[i] = i + 1;
+// }
+// for (var i = 0; i < n; i++)
+//     console.log(tringle); 
+
+/*question 22 */
+var NonZero = [];
+var i = 0;
+NonZero[i] = +prompt("enter number:");
+while (NonZero[i] != 0) {
+    i++;
+    NonZero[i] = +prompt("enter number:");
 }
-console.log("the first cell in the array " + arr[0]);
-console.log("the last cell in the array " + arr[4]);
-console.log("the array is :")
-for (var i = 0; i < 5; i++)
-    console.log(arr[i]);
+for (i = (NonZero.length - 1); i >= 0; i--)
+    console.log(NonZero[i]);
 
-console.log("the array in the oppsite is :")
-for (i = 4; i >= 0; i--)
-    console.log(arr[i]);
+/*Challenge q1 */
